d5336c33c5a7c88164e1e1c048349070e7b8294a
div0364 <divyam.d.sinha@gmail.com>
2024-12-31 14:36:31 +0530

    tenth commit

--- Backend/Frontend/vite.config.js
+++ Backend/Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:5002",
+        target: "https://chatapp-1-m6sv.onrender.com",
         changeOrigin: true,
       },
     },
